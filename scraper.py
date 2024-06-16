0f7b9c46ed05259d3cf8502766153f0236171f68
alyssadsouza <alyssa.dsouza63@gmail.com>
2024-06-16 17:18:25 -0400

    🎨 Rename self.data to self.game_deals

--- scraper.py
+++ scraper.py
@@ -2,6 +2,7 @@ import time
 from playwright.sync_api import TimeoutError
 import json
 
+
 class Scraper:
     def __init__(self, playwright, slow_mo, headless):
         self.browser = playwright.chromium.launch(headless=headless, slow_mo=slow_mo)
@@ -44,34 +45,37 @@ class SwitchScraper(Scraper):
         super().__init__(playwright=playwright, slow_mo=slow_mo, headless=headless)
         self.deals_url = "https://www.nintendo.com/en-ca/store/games/#p=1&sort=df&f=topLevelFilters&topLevelFilters=Deals"
         self.query_response_endpoint = "store_game_en_ca/query"
-        self.data = []
+        self.game_deals = []
 
     def load_more_results(self) -> bool:
+        """Scrolls to the bottom of the game deals page to click 'Load more results' button."""
         try:
             self.page.get_by_text("Load more results").click(timeout=10000)
             return True
+
         except TimeoutError:
             print("No more results to load.")
             return False
+
         except Exception as e:
             raise Exception(f"Error occurred while loading more results: {e}")
 
     def get_game_deals(self) -> list[dict]:
+        """Opens the Game Deals page and scrapes all game deal data."""
         self.page.goto(self.deals_url)
+
         try:
-            # Get the API response data
-            # handle_response called each time new results are loaded
+            # self.handle_response is called each time new results are loaded and saves the response data
             while self.load_more_results():
                 time.sleep(4)  # Pause for 4 seconds after each scroll
                 break
-            
-            # self.data contains all data received at query_response_endpoint
-            return self.data
+
         except Exception as e:
             print(f"Error occurred while scrolling: {e}")
-            return []
 
-        # Return the data
+        finally:
+            # self.game_deals contains all data received at query_response_endpoint
+            return self.game_deals
 
     def handle_response(self, response):
         if self.query_response_endpoint in response.url:
@@ -84,4 +88,4 @@ class SwitchScraper(Scraper):
                 }
             )
             data = json.loads(response.body())
-            self.data += data["hits"]
+            self.game_deals += data["hits"]
